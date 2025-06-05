4cb359209f49d2ecf356a72f6b3ff59110ed6128
Nadtak28 <nadeemtakleh@gmail.com>
2025-06-05 13:11:27 +0300

    first commit

---
+++ adventra/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
+
+
+ {/* <div>
+          nademo d5el 3eono
+        </div>
+
+        <div>
+          mouazo d5el jfono
+        </div>
+        <div>
+          marhofo d5el z3nefo
+        </div>
+          <div>
+              mohannad d5el sh3rato
+          </div>
+          <div>
+              manzora d5el tslekato
+          </div> */}
